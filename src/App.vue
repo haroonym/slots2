96edb4b2b355fcb2a8cf7d0480e4e6820d2f741c
haroonym <71009111+haroonym@users.noreply.github.com>
2022-01-12 13:11:44 +0100

    fertig

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container mt-5" id="app">
+    <Catogram :cats="cats">
+      <template #boss="{item}">
+        <h5>{{ item.name }}</h5>
+        <img :src="item.image" width="200" alt="" />
+      </template>
+      <template #employees="{item}">
+        <div>
+          <h5>{{ item.name }}</h5>
+          <img :src="item.image" width="200" alt="" />
+          <p class="mt-2 fw-bold">Job role: {{ item.role }}</p>
+        </div>
+      </template>
+    </Catogram>
+  </div>
+</template>
+
+<script>
+import cats from '@/assets/cats.json';
+
+import Catogram from '@/components/Catogram.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Catogram,
+  },
+  data() {
+    return {
+      cats,
+      // cats: [
+      //   {
+      //     name: 'Franz',
+      //     role: 'Sales',
+      //     image: 'images/franz.png',
+      //   },
+      //   {
+      //     name: 'Pizz',
+      //     role: 'Research',
+      //     image: 'images/pizz.png',
+      //   },
+      //   {
+      //     name: 'Jeremy',
+      //     role: 'Boss',
+      //     image: 'images/jeremy.png',
+      //   },
+      //   {
+      //     name: 'Stella',
+      //     role: 'Customer Support',
+      //     image: 'images/stella.png',
+      //   },
+      // ],
+    };
+  },
+};
+</script>
---
+++ src/components/Catogram.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h3 class="fw-bold mb-5">Scratchy Inc.</h3>
+    <div>
+      <h4 class="fw-bold">Boss</h4>
+      <slot name="boss" :item="boss"></slot>
+    </div>
+    <h4 class="fw-bold mt-5">Employees</h4>
+    <div class="d-flex flex-wrap">
+      <div class="m-1" v-for="(employee, i) of employees" :key="i">
+        <slot name="employees" :item="employee">
+          <span>{{ cat.name }}</span>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cats: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      boss: this.cats.find((el) => el.role == 'Boss'),
+      employees: this.cats.filter((el) => el.role != 'Boss'),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
